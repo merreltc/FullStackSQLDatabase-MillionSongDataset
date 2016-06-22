abdae3ef6e04bef0c4f79c146fabb00722177bba
Panfilwk <walt@panfil.com>
2016-06-22 15:16:14 -0400

    Table creation things and stuff

--- Database Creation Queries.sql
+++ Database Creation Queries.sql
@@ -1,17 +1,35 @@
 --Creates the initial database
 CREATE DATABASE MusicAnalysis
-
---Initializes using the correct database
+GO
 USE MusicAnalysis
 GO
-
---Creates useful types for the database
+--Creates rules for types
+CREATE RULE valid_index AS
+	@index > 0 AND @index < 1;
+GO
+--Creates types for the database
 CREATE TYPE enID FROM char(18);
 CREATE TYPE mbID FROM uniqueidentifier;
 CREATE TYPE [7dID] FROM int;
+CREATE TYPE [index] FROM decimal;
+EXEC sp_bindrule valid_index, 'index';
 
---Creates all the tables in the database
+GO
+--Creates the Song table
 CREATE TABLE Song
-	(echoNestId enID PRIMARY KEY, musicBrainzID mbID UNIQUE, sevenDigitalID [7dID] UNIQUE, 
-	title varchar(100), [year] int,  release varchar(200,
-	CONSTRAINT valid_year CHECK([year] < YEAR(GETDATE()) && [year] > 1800));
+	(echonest_id enID PRIMARY KEY, musicbrainz_id mbID UNIQUE, sevendigital_id [7dID] UNIQUE, 
+	title varchar(100), [year] int,  release varchar(200), loudness decimal, hotttnesss [index],
+	tempo float, [key] int, mode int, start decimal,
+	CONSTRAINT valid_year CHECK([year] < YEAR(GETDATE()) AND [year] > 1800));
+
+GO
+--Creates the Artist table
+CREATE TABLE Artist
+	(echonest_id enID PRIMARY KEY, musicbrainz_id mbID UNIQUE, 
+	name varchar(200), hotttnesss [index], familiarity [index]);
+
+GO
+--Creates the Performance table
+CREATE TABLE Performance
+	(artist enID REFERENCES Artist(echonest_id), song enID REFERENCES Song(echonest_id),
+	genre varchar(200))
