5517173071290ec39077a06bb7aa398b7a1cf5cf
Trinity Merrell <merreltc@rose-hulman.edu>
2016-06-22 15:11:11 -0400

    files

--- html/import.php
+++ html/import.php
@@ -11,11 +11,138 @@
   	   die('Could not connect: ' . mysqli_error($con));
  	}
 
- 	mysqli_select_db($con,"testdb");
- 	$sql="SELECT * FROM test_table WHERE id='".$q."'";
- 	$result = mysqli_query($con,$sql);
+ 	// Make MillionSongs the current database
+	$db_selected = mysql_select_db($con, 'testdb');
+	if (!$db_selected) {
+		// Create database
+		 $sql = "CREATE DATABASE testdb";
+		if (mysqli_query($con, $sql)) {
+		    echo "Database created successfully";
+		} else {
+		    echo "Error creating database: " . mysqli_error($con);
+		}
+	}
 
-	echo "</table>";
+	// sql to create table
+	$sql = "CREATE TABLE IF NOT EXISTS Song (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP
+	)";
+
+	if (mysqli_query($con, $sql)) {
+	    echo "Table MyGuests created successfully";
+	} else {
+	    echo "Error creating table: " . mysqli_error($con);
+	}
+
+	// sql to create table
+	$sql = "CREATE TABLE IF NOT EXISTS Artist (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP
+	)";
+
+	if (mysqli_query($con, $sql)) {
+	    echo "Table MyGuests created successfully";
+	} else {
+	    echo "Error creating table: " . mysqli_error($con);
+	}
+
+	// sql to create table
+	$sql = "CREATE TABLE IF NOT EXISTS Listener (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP
+	)";
+
+	if (mysqli_query($con, $sql)) {
+	    echo "Table MyGuests created successfully";
+	} else {
+	    echo "Error creating table: " . mysqli_error($con);
+	}
+
+	// sql to create table
+	$sql = "CREATE TABLE IF NOT EXISTS Performs (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP
+	)";
+
+	if (mysqli_query($con, $sql)) {
+	    echo "Table MyGuests created successfully";
+	} else {
+	    echo "Error creating table: " . mysqli_error($con);
+	}
+
+	// sql to create table
+	$sql = "CREATE TABLE IF NOT EXISTS Tag (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP
+	)";
+
+	if (mysqli_query($con, $sql)) {
+	    echo "Table MyGuests created successfully";
+	} else {
+	    echo "Error creating table: " . mysqli_error($conn);
+	}
+
+	// sql to create table
+	$sql = "CREATE TABLE IF NOT EXISTS Favorite_Songs (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP
+	)";
+
+	if (mysqli_query($con, $sql)) {
+	    echo "Table MyGuests created successfully";
+	} else {
+	    echo "Error creating table: " . mysqli_error($con);
+	}
+
+	// sql to create table
+	$sql = "CREATE TABLE IF NOT EXISTS Listens_To_Artist (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP
+	)";
+
+	if (mysqli_query($con, $sql)) {
+	    echo "Table MyGuests created successfully";
+	} else {
+	    echo "Error creating table: " . mysqli_error($con);
+	}
+
+	// sql to create table
+	$sql = "CREATE TABLE IF NOT EXISTS Listens_To_Song (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP
+	)";
+
+	if (mysqli_query($con, $sql)) {
+	    echo "Table MyGuests created successfully";
+	} else {
+	    echo "Error creating table: " . mysqli_error($con);
+	}
+
  	mysqli_close($con);
  ?>
 </body>
