ab35a3f3c98761b8ac88b57ca3d2f9657677c3cf
Walt Panfil <walt@panfil.com>
2016-07-18 13:07:01 -0400

    Some work done on debugging RIYL query, free of syntax errors but does not function correctly

--- RIYL Query.php
+++ RIYL Query.php
@@ -1,26 +1,32 @@
 <?php
 function riylSong($songname) {
-	$con = mysqli_connect('localhost', 'superuser', 'superp@$$123', 'projecttest');
+	$con = mysqli_connect('localhost', 'root', '10826294', 'projecttest');
 	if (!$con) {
 		die("Could not connect: " . mysqli_error($con) . "\n");
 	}
 	$sql = "SELECT echonest_id
 		FROM Song
-		WHERE title = {$songname}"
+		WHERE title = '{$songname}'";
 	$result = mysqli_query($con, $sql);
 	if(!$result) {
-		die("There is no song with that title");
+		die("There is no song with that title\n");
 	}
 	$song_id = mysqli_fetch_array($result, MYSQLI_ASSOC)["echonest_id"];
+	echo $song_id . "\n";
 	$sql = "SELECT s.title AS Song, SUM(l.playcount) AS Weight
 		FROM Song AS s, Listens_To_Song AS l
-		WHERE s.echonest_id = l.song AND l.id IN (
-			SELECT DISTINCT id
+		WHERE s.echonest_id = l.song AND l.listener IN (
+			SELECT DISTINCT listener
 			FROM Listens_To_Song
-			WHERE song = {$song_id})
+			WHERE song IN (
+					SELECT echonest_id
+					FROM Song
+					WHERE title = '{$songname}')
 		GROUP BY Song
-		ORDER BY Weight"
+		ORDER BY Weight";
 	$result = mysqli_query($con, $sql);
 	return $result;
 }
+
+var_dump(riylSong("A Dustland Fairytale"));
 ?>
