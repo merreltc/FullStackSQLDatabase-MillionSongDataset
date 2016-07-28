137ff51dddca39c90f41b2309c1896aeee503c63
Trinity Merrell <merrelt@rose-hulman.edu>
2016-07-28 16:37:18 -0400

    Transactions for tags in the process of implementation/testing

--- html/php/try_tag.php
+++ html/php/try_tag.php
@@ -15,36 +15,58 @@ switch(tag_type) {
 	case "song":
 	// Check for song in database first
 	$sql="BEGIN TRANSACTION
+		IF NOT EXISTS(SELECT * FROM Song s, Artist a WHERE s.artist = a.echonest_id AND s.title = '".$song."' AND a.name = '".$artist."')
+		BEGIN
+		RAISERROR('Song does not exists in database.', 10, 1)
+		ROLLBACK TRANSACTION;
+		END
+
 		IF EXISTS (SELECT * FROM Tag WHERE song = '".$song."' AND tag = '".$tag."')
 		BEGIN
 		RAISERROR('Song tag already exists.', 10, 1)
 		ROLLBACK TRANSACTION;
 		END
+
+		DECLARE @song char(18)
+		SELECT @song = s.echonest_id
+			FROM Song s, Artist a WHERE s.artist = a.echonest_id AND s.title = '".$song."' AND a.name = '".$artist."'
+
 		INSERT INTO Tag(song, tag)
-			VALUES('".$song."' , '".$tag."')
+			VALUES(@song, '".$tag."')
 		COMMIT TRANSACTION;";
 	if (mysqli_query($con,$sql)) {
 		echo "<p>Song tag pending, please wait approx. 72 hours for tag to appear on the site.</p>";
 	} else {
-		echo "<p>Error tagging song. Please try again or <a href='#'>contact us.</a></p>";
+		echo "<p>Error tagging song: ".mysqli_error($con)." Please try again or <a href='#'>contact us.</a></p>";
 	}
 	break;
 
 	case "artist":
 	// Check for song in database first
 	$sql="BEGIN TRANSACTION
+		IF NOT EXISTS(SELECT * FROM Artist WHERE name = '".$artist."')
+		BEGIN
+		RAISERROR('Artist does not exists in database.', 10, 1)
+		ROLLBACK TRANSACTION;
+		END
+
 		IF EXISTS (SELECT * FROM Artist_Tag WHERE artist = '".$artist."' AND tag = '".$tag."')
 		BEGIN
 		RAISERROR('Artist tag already exists.', 10, 1)
 		ROLLBACK TRANSACTION;
 		END
+
+		DECLARE @artist char(18)
+		SELECT @artist = echonest_id
+			FROM Artist WHERE name = '".$artist."'
+
 		INSERT INTO Artist_Tag(artist, tag)
-			VALUES('".$artist."' , '".$tag."')
+			VALUES(@artist, '".$tag."')
 		COMMIT TRANSACTION;";
 	if (mysqli_query($con,$sql)) {
 		echo "<p>Artist tag pending, please wait approx. 72 hours for tag to appear on the site.</p>";
 	} else {
-		echo "<p>Error tagging artist. Please try again or <a href='#'>contact us.</a></p>";
+		echo "<p>Error tagging artist: ".mysqli_error($con)." Please try again or <a href='#'>contact us.</a></p>";
 	}
 	break;
 	break;
