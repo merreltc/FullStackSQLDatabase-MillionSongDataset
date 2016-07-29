d261de502aa60a60647d1ad597a41f1956d397e6
Trinity Merrell <merrelt@rose-hulman.edu>
2016-07-28 22:03:47 -0400

    Made changes to table in recommend_song.php

--- html/php/recommend_song.php
+++ html/php/recommend_song.php
@@ -16,18 +16,14 @@ if (mysqli_num_rows($result) > 0) {
 	<tr>
 		<th>Title</th>
 		<th>Artist</th>
-		<th>Genre</th>
 		<th>Album</th>
-		<th>Release Year</th>
 	</tr>";
 
 	while($row = mysqli_fetch_array($result)) {
 		echo "<tr>";
 		echo "<td>" . $row['title'] . "</td>";
 		echo "<td>" . $row['name'] . "</td>";
-		echo "<td>" . $row['genre'] . "</td>";
 		echo "<td>" . $row['album'] . "</td>";
-		echo "<td>" . $row['release_year'] . "</td>";
 		echo "</tr>";
 	}
 	echo "</table>";
